34cbb7a02cb3072c5c189d6eb09b600748c5ceef
David Medina <dmedina@wilsonelectronics.com>
2020-04-18 17:01:22 -0600

    update 1
    added shuffle for quotes
    added modual

--- javascript.js
+++ javascript.js
@@ -1,16 +1,37 @@
 slideIndex = 0;
+modal = false;
 var quoteStart
 const quoteEngine = () => {
     var i;
     var slides = document.getElementsByClassName("quoteSlides");
+    var rand = Math.floor((Math.random() * slides.length));
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
 
     slideIndex++;
     if (slideIndex > slides.length) {
+        slides[rand]
         slideIndex = 1, true;
     };
-    slides[slideIndex - 1].style.display = "block";
-    quoteStart = setTimeout(quoteEngine, 15000);
+    slides[rand].style.display = "block";
+    quoteStart = setTimeout(quoteEngine, 10000);
 };
+
+const vovoModal = () => {
+    modal = true;
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
+};
+
+const modalClose = () => {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+};
+
+window.addEventListener("click", function(event) {
+    var modal = document.getElementById("myModal");
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+});
