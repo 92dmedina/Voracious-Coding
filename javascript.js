2b2ad56cd303ea6e953c0e7bcffd3379517b1714
David Medina <dmedina@wilsonelectronics.com>
2020-04-19 12:00:15 -0600

    update 3
    added clock
    moved calendar into modal
    did css
    clean code

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 slideIndex = 0;
-modal = false;
+var calOn = false;
 var quoteStart
+    //quote engine
 const quoteEngine = () => {
     var i;
     var slides = document.getElementsByClassName("quoteSlides");
@@ -17,9 +18,8 @@ const quoteEngine = () => {
     slides[rand].style.display = "block";
     quoteStart = setTimeout(quoteEngine, 10000);
 };
-
+//Open close modal
 const vovoModal = () => {
-    modal = true;
     var modal = document.getElementById("myModal");
     modal.style.display = "block";
 };
@@ -28,10 +28,49 @@ const modalClose = () => {
     var modal = document.getElementById("myModal");
     modal.style.display = "none";
 };
-
+//close modal outside div
 window.addEventListener("click", function(event) {
     var modal = document.getElementById("myModal");
     if (event.target == modal) {
         modal.style.display = "none";
     }
 });
+//Open calendar
+const calOpen = () => {
+    cal = document.getElementById("vovoCalendar");
+    if (calOn === false) {
+        cal.style.display = "block";
+        calOn = true;
+    } else {
+        cal.style.display = "none";
+        calOn = false;
+    };
+};
+//Clock
+const showTime = () => {
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
